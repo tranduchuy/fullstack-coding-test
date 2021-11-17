40e215403f3c7b6d52c324f18aa6011acf2d22b8
tranduchuy <thewish.it@gmail.com>
2021-11-17 21:27:56 +0700

    ADD init login and sign up pages

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoginPage = (): JSX.Element => {
+    console.log('this is login page');
+    return (
+        <div>Login page</div>
+    )
+}
+
+export default LoginPage;
---
+++ pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SignUpPage = (): JSX.Element => {
+    console.log('this is sign up page');
+    return (
+        <div>sign up page</div>
+    )
+}
+
+export default SignUpPage;
